5ba3b566b6df445df4613bfdfe2db4b2fcf37073
Trinh Duc Thien <trdthien@Trinhs-MacBook-Pro.local>
2019-03-28 00:39:13 -0400

    dependencies

--- config/bundles.php
+++ config/bundles.php
@@ -7,4 +7,6 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
+    JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
 ];
